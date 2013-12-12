207e4128f61748eb2bd6979385462b4ebdc89560
Ammaross Danan <ammaross@users.noreply.github.com>
2013-12-12 11:07:42 -0700

    Update ownerOldest.php
    Retained for compatibility.
    Updated with full PHP open tags.
    Cleaned up formatting a bit.

--- ownerOldest.php
+++ ownerOldest.php
@@ -1,8 +1,7 @@
-
-<?
+<?php
 #shows who owns the oldest ticket
 
- $query1 = "
+$query1 = "
 SELECT 
 HD_TICKET.ID as ID, 
 HD_TICKET.TITLE as Title, 
@@ -19,47 +18,38 @@ LEFT JOIN USER O ON (O.ID = HD_TICKET.OWNER_ID) LEFT JOIN USER S ON (S.ID = HD_T
 WHERE ((HD_STATUS.NAME not like '%Closed%') and O.FULL_NAME is not null)
 ORDER BY HD_TICKET.ID
 Limit 1
-
 ";
 
 
- $result1 = mysql_query($query1);
- $num = mysql_numrows($result1);
- $i = 0;
- while ($i < $num)
- 	{
-        $ID = mysql_result($result1,$i,"ID");
+$result1 = mysql_query($query1);
+$num = mysql_numrows($result1);
+$i = 0;
+while ($i < $num)
+{
+	$ID = mysql_result($result1,$i,"ID");
 	$Title = mysql_result($result1,$i,"Title");
-        $Status = mysql_result($result1,$i,"Status");        
-        $Department = mysql_result($result1,$i,"Department");
-        $Created = mysql_result($result1,$i,"Created");
-        $Modified = mysql_result($result1,$i,"Modified");
-        $Priority = mysql_result($result1,$i,"Priority");
-        $Owner = mysql_result($result1,$i,"Owner");	
-        $Submitter = mysql_result($result1,$i,"Submitter");
-
+	$Status = mysql_result($result1,$i,"Status");        
+	$Department = mysql_result($result1,$i,"Department");
+	$Created = mysql_result($result1,$i,"Created");
+	$Modified = mysql_result($result1,$i,"Modified");
+	$Priority = mysql_result($result1,$i,"Priority");
+	$Owner = mysql_result($result1,$i,"Owner");	
+	$Submitter = mysql_result($result1,$i,"Submitter");
 
 	$ID = stripslashes($ID);
-        $Title = stripslashes($Title);
-        $Status = stripslashes($Status);
-        $Department = stripslashes($Department);
-        $Created = stripslashes($Created);	
-        $Modified = stripslashes($Modified);
-        $Priority = stripslashes($Priority);
-        $Owner = stripslashes($Owner);
-        $Submitter = stripslashes($Submitter);
+	$Title = stripslashes($Title);
+	$Status = stripslashes($Status);
+	$Department = stripslashes($Department);
+	$Created = stripslashes($Created);	
+	$Modified = stripslashes($Modified);
+	$Priority = stripslashes($Priority);
+	$Owner = stripslashes($Owner);
+	$Submitter = stripslashes($Submitter);
 
+	print("<img src='includes/img/50px_golden-star.jpg'>Congratulations $Owner, you own the oldest ticket"
+		. " (<a href='http://$KaceBoxDNS/adminui/ticket.php?ID=$ID' target='_blank'>$ID</a>)!");
 
-
-echo "<img src='includes/img/50px_golden-star.jpg'>Congratulations $Owner, you own the 
-oldest ticket (<a 
-href='http://$KaceBoxDNS/adminui/ticket.php?ID=$ID' 
-target='_blank'>$ID</a>)!";
-
-$i ++;
-
-        }
-                  
-
+	$i++;
+}
 ?>
 
