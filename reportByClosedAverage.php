5b81a2026d54e2a5ab664c7c967bd243d83df455
Ammaross Danan <ammaross@users.noreply.github.com>
2013-12-12 11:17:42 -0700

    Update reportByClosedAverage.php
    Cleaned up formatting.
    Added full PHP open tags.

--- reportByClosedAverage.php
+++ reportByClosedAverage.php
@@ -1,25 +1,28 @@
-<?
+<?php
 include ('includes/config.php');
 
-$query="Select
+$query="
+SELECT
   Avg(s1.cct) As max_cct
-From
-  (Select
+FROM
+  (SELECT
     Count(HD_TICKET.ID) As cct
-  From
+  FROM
     HD_STATUS Inner Join
-  HD_TICKET On HD_TICKET.HD_STATUS_ID = HD_STATUS.ID
-  where (HD_STATUS.NAME = 'Closed')
-  and (
+  HD_TICKET ON HD_TICKET.HD_STATUS_ID = HD_STATUS.ID
+  WHERE (HD_STATUS.STATE LIKE '%Closed%')
+  AND (
        	(HD_STATUS.NAME not like '%spam%')
-AND (TIME_CLOSED >= ( CURDATE() - INTERVAL 30 DAY ))
-)
-  Group By
-    Date(TIME_CLOSED)) As s1";
-
+		AND (TIME_CLOSED >= ( CURDATE() - INTERVAL 30 DAY ))
+	)
+  GROUP BY
+    Date(TIME_CLOSED)
+  ) As s1
+";
 
 $result = mysql_query($query);
-if (!$result) {
+if (!$result)
+{
     echo 'Could not run query: ' . mysql_error();
     exit;
 }
@@ -27,15 +30,8 @@ $row = mysql_fetch_row($result);
 $theAverage = $row[0];
 $theAverage = round($theAverage, 2);
 
-
 #echo "<h1>$theAverage</h1>Average closed ";
 
 $averageClosed = $row[0];
 echo "</td>";
-
-
-
-
-
-
 ?>
