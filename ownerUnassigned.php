dca74b3a393e09e5bff2a9f994331f625a1b5d16
Ammaross Danan <ammaross@users.noreply.github.com>
2013-12-12 11:12:20 -0700

    Update ownerUnassigned.php
    Cleaned up formatting a bit.
    Added full PHP open tags.

--- ownerUnassigned.php
+++ ownerUnassigned.php
@@ -1,119 +1,104 @@
-<?
+<?php
 $user="Unassigned";
 ?>
 
-<h2><? echo $user ?> support tickets</h2>
+<h2><?php echo $user ?> Support Tickets</h2>
 <table class="table table-striped table-bordered table-condensed">
   <thead>
     <tr>
       <th class=span2>Ticket ID</th>
+	  <th class=span1>Queue</th>
       <th class=span2>Title</th>
-        <th class=span2>Department</th>
-        <th class=span2>Submitter</th>
-
-        
-        
-        <th class=span1>Created</th>
-        <th class=span1>Modified</th>
-
-
-    </tr>
+      <th class=span2>Custom Field 0</th><!--Original author used this field for Department-->
+      <th class=span2>Submitter</th>
+      <th class=span1>Created</th>
+      <th class=span1>Modified</th>
+	</tr>
   </thead>
   <tbody>
 
-
-
-<?
-
- $query1 = "
+  
+<?php
+$query1 = "
 SELECT 
 HD_TICKET.ID as ID, 
 HD_TICKET.TITLE as Title, 
 HD_STATUS.NAME AS Status, 
-HD_TICKET.CUSTOM_FIELD_VALUE0 as Department, 
+HD_TICKET.CUSTOM_FIELD_VALUE0 as Cust0, 
 HD_TICKET.CREATED as Created, 
 HD_TICKET.MODIFIED as Modified, 
 HD_PRIORITY.NAME AS Priority, 
+HD_QUEUE.NAME As QueueName,
 O.FULL_NAME AS Owner, 
 S.FULL_NAME AS Submitter  
 FROM HD_TICKET  JOIN HD_STATUS ON (HD_STATUS.ID = HD_TICKET.HD_STATUS_ID) 
 JOIN HD_PRIORITY ON (HD_PRIORITY.ID = HD_TICKET.HD_PRIORITY_ID) 
 LEFT JOIN USER O ON (O.ID = HD_TICKET.OWNER_ID) LEFT JOIN USER S ON (S.ID = HD_TICKET.SUBMITTER_ID) 
-WHERE (HD_TICKET.HD_QUEUE_ID = $mainQueue) AND 
+LEFT JOIN HD_QUEUE ON (HD_QUEUE.ID=HD_TICKET.HD_QUEUE_ID)
+WHERE (HD_TICKET.HD_QUEUE_ID > 0) AND 
 ((O.FULL_NAME is null) AND 
 (HD_STATUS.NAME not like '%Closed%'))  
 ORDER BY CREATED desc
-
-
 ";
 
 
- $result1 = mysql_query($query1);
- $num = mysql_numrows($result1);
- $i = 0;
- while ($i < $num)
- 	{
-        $ID = mysql_result($result1,$i,"ID");
+$result1 = mysql_query($query1);
+$num = mysql_numrows($result1);
+$i = 0;
+while ($i < $num)
+{
+	$ID = mysql_result($result1,$i,"ID");
 	$Title = mysql_result($result1,$i,"Title");
-        $Status = mysql_result($result1,$i,"Status");        
-        $Department = mysql_result($result1,$i,"Department");
-        $Created = mysql_result($result1,$i,"Created");
-        $Modified = mysql_result($result1,$i,"Modified");
-        $Priority = mysql_result($result1,$i,"Priority");
-        $Owner = mysql_result($result1,$i,"Owner");	
-        $Submitter = mysql_result($result1,$i,"Submitter");
+	$Status = mysql_result($result1,$i,"Status");        
+	$Cust0 = mysql_result($result1,$i,"Cust0");
+	$QueueName = mysql_result($result1,$i,"QueueName");
+	$Created = mysql_result($result1,$i,"Created");
+	$Modified = mysql_result($result1,$i,"Modified");
+	$Priority = mysql_result($result1,$i,"Priority");
+	$Owner = mysql_result($result1,$i,"Owner");	
+	$Submitter = mysql_result($result1,$i,"Submitter");
 
 
 	$ID = stripslashes($ID);
-        $Title = stripslashes($Title);
-        $Status = stripslashes($Status);
-        $Department = stripslashes($Department);
-        $Created = stripslashes($Created);	
-        $Modified = stripslashes($Modified);
-        $Priority = stripslashes($Priority);
-        $Owner = stripslashes($Owner);
-        $Submitter = stripslashes($Submitter);
+	$Title = stripslashes($Title);
+	$Status = stripslashes($Status);
+	$Cust0 = stripslashes($Cust0);
+	$QueueName = stripslashes($QueueName);
+	$Created = stripslashes($Created);	
+	$Modified = stripslashes($Modified);
+	$Priority = stripslashes($Priority);
+	$Owner = stripslashes($Owner);
+	$Submitter = stripslashes($Submitter);
 
 
+	$StatusSpan="";
+	if ($Status=="Stalled")
+	{
+		$StatusSpan="<span class='label label-warning'>$Status</span>";
+	}
 
+	$PriortySpan="";
+	if ($Priority=="High")
+	{
+		$PriortySpan="<span class='label label-important'><i class='icon-exclamation-sign icon-white'></i>High</span>";
+	}
 
+	if ($Priority=="Low")
+	{
+		$PriortySpan="<span class='label'>Low</span>";
+	}
 
-$StatusSpan="";
-if ($Status=="Stalled")
-        {
-        $StatusSpan="<span class='label label-warning'>$Status</span>";
-        }
+	echo "<tr><td><a href='http://$KaceBoxDNS/adminui/ticket.php?ID=$ID' target='_blank'>$ID</a> $StatusSpan $PriortySpan</td> \n";
+	echo "<td>$QueueName</td> \n";
+	echo "<td>$Title</td>\n";
+	echo "<td>$Cust0</td> \n";
+	echo "<td>$Submitter</td> \n";
+	echo "<td>$Created</td> \n";
+	echo "<td>$Modified</td> \n";
 
+	echo "</tr> \n";
+	$i++;
+}
 
-$PriortySpan="";
-if ($Priority=="High")
-        {
-        $PriortySpan="<span class='label label-important'><i class='icon-exclamation-sign icon-white'></i>High</span>";
-        }
-
-if ($Priority=="Low")
-        {
-        $PriortySpan="<span class='label'>Low</span>";
-        }
-
-
-echo "<tr><td><a href='http://$KaceBoxDNS/adminui/ticket.php?ID=$ID' target='_blank'>$ID</a> $StatusSpan $PriortySpan</td> \n";
-echo "<td>$Title</td>\n";
-echo "<td>$Department</td> \n";
-echo "<td>$Submitter</td> \n";
-
-
-
-
-echo "<td>$Created</td> \n";
-echo "<td>$Modified</td> \n";
-
-echo "</tr> \n";
-$i ++;
-
-        }
-                      
 echo "</tbody></table> \n";
-
 ?>
-
