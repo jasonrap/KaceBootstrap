067bfb7787d0ea404356f5d841fe14ba81e8026b
Ammaross Danan <ammaross@users.noreply.github.com>
2013-12-12 11:30:35 -0700

    Added bail-out routine if not configured.
    Cleaned up formatting.
    Added full PHP open tags.
    Added not-configured bail-out check at the top.

--- webTickets.php
+++ webTickets.php
@@ -1,3 +1,12 @@
+<?php
+#NOTE - you will have to change the queue ID number below to match your setup.  
+$webTicketsQueue=0;
+#end edit
+
+if ( $webTicketsQueue == 0 ) // didn't configure? just skip
+	return;
+?>
+
 <h2>Website tickets</h2>
 <table class="table table-striped table-bordered table-condensed">
   <thead>
@@ -6,24 +15,14 @@
       <th class=span2>Title</th>
         <th class=span2>Type</th>
         <th class=span2>Submitter</th>
-
-        
         <th class=span2>Owner</th>
         <th class=span1>Created</th>
         <th class=span1>Modified</th>
-
-
     </tr>
   </thead>
   <tbody>
 
-
-
-<?
-
-#NOTE - you will have to change the queue ID number below to match your setup.  
-$webTicketsQueue="";
-#end edit
+<?php
 
 $query1 = "
 SELECT HD_TICKET.ID as ID, 
@@ -46,68 +45,59 @@ ORDER BY ID desc
 ";
 
 
- $result1 = mysql_query($query1);
- $num = mysql_numrows($result1);
- $i = 0;
- while ($i < $num)
- 	{
-        $ID = mysql_result($result1,$i,"ID");
-	    $Title = mysql_result($result1,$i,"Title");
-        $Status = mysql_result($result1,$i,"Status");        
-        $Type = mysql_result($result1,$i,"Type");
-        $Created = mysql_result($result1,$i,"Created");
-        $Modified = mysql_result($result1,$i,"Modified");
-        $Priority = mysql_result($result1,$i,"Priority");
-        $Owner = mysql_result($result1,$i,"Owner");	
-        $Submitter = mysql_result($result1,$i,"Submitter");
+$result1 = mysql_query($query1);
+$num = mysql_numrows($result1);
+$i = 0;
+while ($i < $num)
+{
+	$ID = mysql_result($result1,$i,"ID");
+	$Title = mysql_result($result1,$i,"Title");
+	$Status = mysql_result($result1,$i,"Status");        
+	$Type = mysql_result($result1,$i,"Type");
+	$Created = mysql_result($result1,$i,"Created");
+	$Modified = mysql_result($result1,$i,"Modified");
+	$Priority = mysql_result($result1,$i,"Priority");
+	$Owner = mysql_result($result1,$i,"Owner");	
+	$Submitter = mysql_result($result1,$i,"Submitter");
 
+	$ID = stripslashes($ID);
+	$Title = stripslashes($Title);
+	$Status = stripslashes($Status);
+	$Type = stripslashes($Type);
+	$Created = stripslashes($Created);	
+	$Modified = stripslashes($Modified);
+	$Priority = stripslashes($Priority);
+	$Owner = stripslashes($Owner);
+	$Submitter = stripslashes($Submitter);
 
-        $ID = stripslashes($ID);
-        $Title = stripslashes($Title);
-        $Status = stripslashes($Status);
-        $Type = stripslashes($Type);
-        $Created = stripslashes($Created);	
-        $Modified = stripslashes($Modified);
-        $Priority = stripslashes($Priority);
-        $Owner = stripslashes($Owner);
-        $Submitter = stripslashes($Submitter);
-
-
-
-
-
-$StatusSpan="";
-if ($Status=="Stalled")
-        {
-        $StatusSpan="<span class='label label-warning'>$Status</span>";
-        }
-
-
-$PriortySpan="";
-if ($Priority=="High")
+	$StatusSpan="";
+	if ($Status=="Stalled")
 	{
-	$PriortySpan="<span class='label label-important'><i class='icon-exclamation-sign icon-white'></i>";
+		$StatusSpan="<span class='label label-warning'>$Status</span>";
 	}
 
-if ($Priority=="Low")
-        {
-        $PriortySpan="<span class='label'>";
-        }
+	$PriortySpan="";
+	if ($Priority=="High")
+	{
+		$PriortySpan="<span class='label label-important'><i class='icon-exclamation-sign icon-white'></i>";
+	}
 
-echo "<tr><td><a href='http://$KaceBoxDNS/adminui/ticket.php?ID=$ID' target='_blank'>$ID</a> $StatusSpan $PriortySpan</span></td> \n";
-echo "<td>$Title</td> \n";
-echo "<td>$Type</td> \n";
-echo "<td>$Submitter</td> \n";
-echo "<td>$Owner</td> \n";
-echo "<td>$Created</td> \n";
-echo "<td>$Modified</td> \n";
+	if ($Priority=="Low")
+	{
+		$PriortySpan="<span class='label'>";
+	}
 
-echo "</tr> \n";
-$i ++;
+	echo "<tr><td><a href='http://$KaceBoxDNS/adminui/ticket.php?ID=$ID' target='_blank'>$ID</a> $StatusSpan $PriortySpan</span></td> \n";
+	echo "<td>$Title</td> \n";
+	echo "<td>$Type</td> \n";
+	echo "<td>$Submitter</td> \n";
+	echo "<td>$Owner</td> \n";
+	echo "<td>$Created</td> \n";
+	echo "<td>$Modified</td> \n";
 
-        }
-                      
+	echo "</tr> \n";
+	$i++;
+}
+
 echo "</tbody></table> \n";
-
 ?>
-
