1d7bf6797d5ca7be54289549427f684e1dd00053
Ammaross Danan <ammaross@users.noreply.github.com>
2013-12-12 11:29:09 -0700

    Cleaned up formatting.
    Cleaned up formatting.

--- reportKaceCurrentOpen.php
+++ reportKaceCurrentOpen.php
@@ -1,18 +1,16 @@
 <?php
-
-
+#start RT queries
+#for coming up with the total for today, average, and max
 $currentlyOpen="";
 include ('includes/config.php');
 
 $theDate = date("Y-m-d");
-
 $theYear = date("Y");
-
 
 $query = "select COUNT(NAME) as counted from HD_TICKET Inner Join HD_STATUS on HD_STATUS.ID = HD_TICKET.HD_STATUS_ID
 where (HD_STATUS.NAME not like '%Spam%')
 AND (HD_STATUS.NAME not like '%Server Status Report%')
-AND (HD_STATUS.NAME not like '%Closed%')
+AND (HD_STATUS.STATE not like '%Closed%')
 order by counted desc";
 
 
@@ -22,16 +20,9 @@ $i = 0;
 
 while ($i < $num)
 {
-
-$currentlyOpen = mysql_result($result,$i,"counted");
-#$name = mysql_result($result,$i,"NAME");
-
-
-
-$i++;
-
+	$currentlyOpen = mysql_result($result,$i,"counted");
+	#$name = mysql_result($result,$i,"NAME");
+	$i++;
 }
-
-
 
 ?>
