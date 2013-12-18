596bbfbc86f91874466a2e1a691ffc3e985dd86a
Ammaross Danan <ammaross@users.noreply.github.com>
2013-12-18 10:45:34 -0700

    Cosmetic table change to add table-head-bordered-bottom

--- openTickets.php
+++ openTickets.php
@@ -1,5 +1,5 @@
 <h2>All Open Tickets</h2>
-<table class="table table-striped table-bordered table-condensed">
+<table class="table table-striped table-bordered table-head-bordered-bottom table-condensed">
   <thead>
     <tr>
       <th class=span1>Ticket ID</th>
