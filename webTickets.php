7bb577839dd211bc24a60ec94cf04dc26d787913
Ammaross Danan <ammaross@users.noreply.github.com>
2013-12-18 10:50:43 -0700

    Cosmetic table change to add table-head-bordered-bottom

--- webTickets.php
+++ webTickets.php
@@ -8,7 +8,7 @@ if ( $webTicketsQueue == 0 ) // didn't configure? just skip
 ?>
 
 <h2>Website tickets</h2>
-<table class="table table-striped table-bordered table-condensed">
+<table class="table table-striped table-bordered table-head-bordered-bottom table-condensed">
   <thead>
     <tr>
       <th class=span1>Ticket ID</th>
