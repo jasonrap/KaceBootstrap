09e7ea7d821935e1bcfdab6f7ad97b1881fcb647
Ammaross Danan <ammaross@users.noreply.github.com>
2013-12-18 10:50:29 -0700

    Cosmetic table change to add table-head-bordered-bottom

--- reportGridByCategory.php
+++ reportGridByCategory.php
@@ -174,7 +174,7 @@ while( ($row = mysql_fetch_assoc($result)) )
 
 
 <h2><?php echo $gridTitle ?></h2>
-<table class="table table-striped table-bordered table-condensed">
+<table class="table table-striped table-bordered table-head-bordered-bottom table-condensed">
   <thead>
     <tr>
       <th class=span2>Category</th>
